e9c8a9c7c88f5dc6e19a81656970332a91699d20
Greyson Richey <greysonrichey@gmail.com>
2020-04-18 13:49:52 -0600

    Add damage effect to enemies

--- scripts/classes/Enemy.gd
+++ scripts/classes/Enemy.gd
@@ -4,15 +4,23 @@ class_name Enemy
 export var id: String
 
 onready var _area2d = $"./Area2D"
+onready var _damage_tween: Tween = $"./DamageTween"
+onready var _sprite = $"./Sprite"
+onready var _sprite_material = _sprite.material
 
 var _current_health: int
 var _damage: int
+var _damage_shader_param: float = 0
 var _health: int
 var _speed: float
 var _value: int
 
 func damage(amount: int)->void:
   _current_health = _current_health - amount
+  _damage_tween.interpolate_property(self, "_damage_shader_param", 1, 0, 0.25, Tween.TRANS_LINEAR, Tween.EASE_IN)
+  
+  if !_damage_tween.is_active():
+    _damage_tween.start()
 
 func get_class()->String:
   return "Enemy"
@@ -36,6 +44,7 @@ func _parse_data()->void:
 func _process(delta)->void:
   if _current_health > 0:
     position = position + (Vector2(_speed, 0) * delta)
+    _sprite_material.set_shader_param("amount", _damage_shader_param)
   else:
     store.dispatch(actions.player_add_gold(_value))
     queue_free()
