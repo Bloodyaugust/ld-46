99ebb9ef28c24dafd3eeda4fb76e7235b29fa7a8
Greyson Richey <greysonrichey@gmail.com>
2020-04-18 21:14:25 -0600

    Add FloatingText class
    - Fix FloatingText script
    - Add money FloatingText indicator on enemy kill
    - Implement bonus gold and slow upgrade effects
    - Improve damage shader
    - Add slow effect to damage shader

--- scripts/classes/Enemy.gd
+++ scripts/classes/Enemy.gd
@@ -1,10 +1,13 @@
 extends Node2D
 class_name Enemy
 
+const SLOW_DURATION_BASE: float = 1.0
+
 export var id: String
 
 onready var _area2d = $"./Area2D"
 onready var _damage_tween: Tween = $"./DamageTween"
+onready var _floating_text: PackedScene = preload("res://doodads/floating_text.tscn")
 onready var _health_bar: ProgressBar = $"./HealthBar"
 onready var _sprite = $"./Sprite"
 onready var _sprite_material = _sprite.material
@@ -13,6 +16,8 @@ var _current_health: int
 var _damage: int
 var _damage_shader_param: float = 0
 var _health: int
+var _slow_amount: float
+var _slow_duration_left: float
 var _speed: float
 var _value: int
 
@@ -36,6 +41,9 @@ func _on_input_event(_viewport, event: InputEvent, _shape_index)->void:
   if event.is_action("game_click") && event.is_action_pressed("game_click"):
     damage(1 + store.state()["player"]["upgrades"].get("click-damage", 0))
 
+    if store.state()["player"]["upgrades"].get("click-slow", 0) > 0:
+      _slow_duration_left = SLOW_DURATION_BASE * store.state()["player"]["upgrades"].get("click-slow", 0)
+
 func _parse_data()->void:
   var _data: Dictionary = DataController.data["enemy"][id]
 #  Eat the data into a first party data structure
@@ -49,10 +57,34 @@ func _parse_data()->void:
 
 func _process(delta)->void:
   if _current_health > 0:
-    position = position + (Vector2(_speed, 0) * delta)
+    if _slow_duration_left > 0:
+      _slow_duration_left -= delta
+      
+      if _slow_amount == 0:
+        _slow_amount = store.state()["player"]["upgrades"].get("click-slow", 0) * 0.1
+    else:
+      _slow_duration_left = 0
+      _slow_amount = 0
+
+    position = position + (Vector2(_speed, 0) * delta) - (Vector2(_speed, 0) * delta * _slow_amount)
     _sprite_material.set_shader_param("amount", _damage_shader_param)
+    if _slow_duration_left > 0:
+      _sprite_material.set_shader_param("slow_amount", _slow_duration_left / (SLOW_DURATION_BASE * store.state()["player"]["upgrades"].get("click-slow", 0)))
+    else:
+      _sprite_material.set_shader_param("slow_amount", 0)
+
   else:
-    store.dispatch(actions.player_add_gold(_value))
+    var _new_floating_text = _floating_text.instance()
+    var _gold_added = _value + store.state()["player"]["upgrades"].get("enemy-kill-bonus-gold", 0)
+
+    store.dispatch(actions.player_add_gold(_gold_added))
+    _new_floating_text.get_node("Label").text = str(_gold_added)
+    _new_floating_text.global_position = global_position - Vector2(0, 10)
+    _new_floating_text.time_to_live = 1
+    _new_floating_text.float_speed = 100
+
+    get_tree().get_root().add_child(_new_floating_text)
+
     queue_free()
 
 func _ready()->void:
--- shaders/damage.shader
+++ shaders/damage.shader
@@ -2,15 +2,17 @@ shader_type canvas_item;
 render_mode blend_mix;
 
 uniform float amount : hint_range(0, 1);
+uniform float slow_amount : hint_range(0, 1);
 uniform vec4 color : hint_color;
+uniform vec4 slow_color : hint_color;
 
 void fragment() {
   COLOR = texture(TEXTURE, UV);
   
-  if (amount > 0.0) {
-    vec3 mixed_colors = mix(vec3(1, 1, 1), vec3(color.r, color.g, color.b), amount);
-    COLOR = COLOR * vec4(mixed_colors.r, mixed_colors.g, mixed_colors.b, 1);
-  }
+  vec3 mixed_colors = mix(vec3(1, 1, 1), vec3(color.r, color.g, color.b), amount);
+  mixed_colors = mix(mixed_colors, vec3(slow_color.r, slow_color.g, slow_color.b), slow_amount - amount);
+  
+  COLOR = COLOR * vec4(mixed_colors.r, mixed_colors.g, mixed_colors.b, 1);
 }
 
 void vertex() {
