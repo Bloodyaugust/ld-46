4bbf98c8099824d3e9a1f73bab130ebf2b724238
Greyson Richey <greysonrichey@gmail.com>
2020-04-20 00:08:05 -0600

    Update filename, info, icon

--- tools/build-and-deploy.sh
+++ tools/build-and-deploy.sh
@@ -18,24 +18,24 @@ mkdir build/win/
 
 echo "EXPORTING FOR LINUX"
 echo "-----------------------------"
-godot --export "Linux/X11" build/linux/ld-46.x86_64 -v
+godot --export "Linux/X11" build/linux/defend-your-plant.x86_64 -v
 echo "EXPORTING FOR OSX"
 echo "-----------------------------"
-godot --export "Mac OSX" build/osx/ld-46.dmg -v
+godot --export "Mac OSX" build/osx/defend-your-plant.dmg -v
 echo "EXPORTING FOR WINDOZE"
 echo "-----------------------------"
-godot --export "Windows Desktop" build/win/ld-46.exe -v
+godot --export "Windows Desktop" build/win/defend-your-plant.exe -v
 echo "-----------------------------"
 
 echo "CHANGING FILETYPE AND CHMOD EXECUTABLE FOR OSX"
 echo "-----------------------------"
 cd build/osx/
-mv ld-46.dmg ld-46-osx-alpha.zip
-unzip ld-46-osx-alpha.zip
-rm ld-46-osx-alpha.zip
-chmod +x ld-46.app/Contents/MacOS/ld-46
-zip -r ld-46-osx-alpha.zip ld-46.app
-rm -rf ld-46.app
+mv defend-your-plant.dmg defend-your-plant-osx-alpha.zip
+unzip defend-your-plant-osx-alpha.zip
+rm defend-your-plant-osx-alpha.zip
+chmod +x defend-your-plant.app/Contents/MacOS/defend-your-plant
+zip -r defend-your-plant-osx-alpha.zip defend-your-plant.app
+rm -rf defend-your-plant.app
 cd ../../
 
 ls -al
@@ -50,6 +50,6 @@ butler login
 
 echo "Pushing builds with Butler"
 echo "-----------------------------"
-butler push build/linux/ synsugarstudio/ld-46:linux-alpha
-butler push build/osx/ synsugarstudio/ld-46:osx-alpha
-butler push build/win/ synsugarstudio/ld-46:win-alpha
+butler push build/linux/ synsugarstudio/defend-your-plant:linux-alpha
+butler push build/osx/ synsugarstudio/defend-your-plant:osx-alpha
+butler push build/win/ synsugarstudio/defend-your-plant:win-alpha
