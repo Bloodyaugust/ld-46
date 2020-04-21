cf6425d5154a1842e91d09c088a73c17fc75b779
Greyson Richey <greysonrichey@gmail.com>
2020-04-20 18:46:26 -0600

    Add score to GameOver screen

--- scripts/controllers/GameOverController.gd
+++ scripts/controllers/GameOverController.gd
@@ -6,8 +6,17 @@ onready var _position_tween: Tween = $"./PositionTween"
 onready var _resolution_label: Label = $"./MarginContainer/VBoxContainer/ResolutionLabel"
 onready var _restart_easy_button: Button = $"./MarginContainer/VBoxContainer/RestartContainer/Easy"
 onready var _restart_normal_button: Button = $"./MarginContainer/VBoxContainer/RestartContainer/Normal"
+onready var _score_label: Label = $"./MarginContainer/VBoxContainer/HBoxContainer/ScoreLabel"
 
-func _hide():
+func _get_score()->float:
+  var _score: float = store.state()["game"]["enemies_killed"] * 4
+
+  _score += store.state()["player"]["gold_earned"]
+  _score *= 0.5 if SpawnController.difficulty == "easy" else 2
+
+  return _score
+
+func _hide()->void:
   _position_tween.interpolate_property(self, "rect_position", Vector2(100, 100), Vector2(100, 1000), 0.25, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 
   if !_position_tween.is_active():
@@ -30,6 +39,7 @@ func _show()->void:
   _enemies_killed_label.text = "Bugs killed: {amount}".format({"amount": store.state()["game"]["enemies_killed"]})
   _gold_earned_label.text = "earned: {amount}".format({"amount": store.state()["player"]["gold_earned"]})
   _resolution_label.text = "You lost!" if store.state()["player"]["health"] <= 0 else "You won!"
+  _score_label.text = "Score: {score}".format({"score": _get_score()})
 
   _position_tween.interpolate_property(self, "rect_position", Vector2(100, 1000), Vector2(100, 100), 0.75, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 
